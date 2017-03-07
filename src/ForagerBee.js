1f46c4f3c565fb61cdbc260157e852b233f22aa6
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-03-07 11:03:22 -0800

    Implement subclasses using es6

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends HoneyMakerBee {
+  constructor() {
+    super();
+
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+
+    this.forage = (treasure) => {
+      this.treasureChest.push(treasure);
+    }
+  }
+
 };
